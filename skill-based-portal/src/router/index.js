8f53d2b78261833a48f7c1fb00b16cdd63be62c0
sherrykjy <helloitsree@gmail.com>
2023-10-29 10:55:23 +0800

    renamed staff files

--- skill-based-portal/src/router/index.js
+++ skill-based-portal/src/router/index.js
@@ -9,37 +9,37 @@ const routes = [
     {
         path: "/overallListing",
         name: "overallListing",
-        component: () => import("../views/overall_listing.vue")
+        component: () => import("../views/staff_overall_listing.vue")
     },
     {
         path: "/My-Applications",
         name: "myApplications",
-        component: () => import("../views/myApplications.vue")
+        component: () => import("../views/staff_myApplications.vue")
     },
     {
         path: "/My-Profile",
         name: "myProfile",
-        component: () => import("../views/myProfile.vue")
+        component: () => import("../views/staff_myProfile.vue")
     },
     {
         path: "/Role-Listing",
         name: "roleListing",
-        component: () => import("../views/role_listing.vue")
+        component: () => import("../views/staff_role_listing.vue")
     },
     {
         path: "/Role-Application",
         name: "roleApplication",
-        component: () => import("../views/role_application.vue")
+        component: () => import("../views/staff_role_application.vue")
     },
     {
         path: "/Application-Confirmation",
         name: "applicationConfirmation",
-        component: () => import("../views/application_confirmation.vue")
+        component: () => import("../views/staff_application_confirmation.vue")
     },
     {
         path: "/View-Application",
         name: "viewApplication",
-        component: () => import("../views/view_application.vue")
+        component: () => import("../views/staff_view_application.vue")
     }
 ]
 
